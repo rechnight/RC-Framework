02dfde8c97caedb253fcd3113a026b6e93b46b2c
rechnight <57619271+rechnight@users.noreply.github.com>
2025-10-13 13:45:50 -0300

    Add files via upload

---
+++ Assets/RCFramework/Tools/Runtime/Events/Lifecycle/LifecycleEvents.cs
@@ -0,0 +1,38 @@
+// --------------- Copyright (C) RC --------------- //
+// STRONG is what happens when you run out of weak! //
+
+using RCFramework.Core;
+using UnityEngine;
+
+namespace RCFramework.Tools
+{
+    public class LifecycleEvents : MonoBehaviour, ILifecycleEvents
+    {
+        public EventChannel UpdateEvent { get; } = new EventChannel();
+        public EventChannel FixedUpdateEvent { get; } = new EventChannel();
+        public EventChannel GUIEvent { get; } = new EventChannel();
+        public EventChannel<bool> ApplicationFocusEvent { get; } = new EventChannel<bool>();
+        public EventChannel<bool> ApplicationPauseEvent { get; } = new EventChannel<bool>();
+        public EventChannel ApplicationQuitEvent { get; } = new EventChannel();
+
+        private LifecycleEvents() { } // Prevent accidental "new"
+
+        public static ILifecycleEvents GetOrCreate()
+        {
+            var existing = FindFirstObjectByType<LifecycleEvents>();
+            if (existing != null)
+                return existing;
+
+            var go = new GameObject(nameof(LifecycleEvents));
+            DontDestroyOnLoad(go);
+            return go.AddComponent<LifecycleEvents>();
+        }
+
+        private void Update() => UpdateEvent?.Raise();
+        private void FixedUpdate() => FixedUpdateEvent?.Raise();
+        private void OnGUI() => GUIEvent?.Raise();
+        private void OnApplicationFocus(bool focus) => ApplicationFocusEvent?.Raise(focus);
+        private void OnApplicationPause(bool pause) => ApplicationPauseEvent?.Raise(pause);
+        private void OnApplicationQuit() => ApplicationQuitEvent?.Raise();
+    }
+}
