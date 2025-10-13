9e37aa7fcbf89c0fb5a148228f62ddcfe8dbdb61
rechnight <57619271+rechnight@users.noreply.github.com>
2025-10-13 09:07:44 -0300

    Add files via upload

---
+++ Assets/RCFramework/Core/Runtime/System/SystemBase.cs
@@ -0,0 +1,36 @@
+// --------------- Copyright (C) RC --------------- //
+// STRONG is what happens when you run out of weak! //
+
+namespace RCFramework.Core
+{
+    public abstract class SystemBase: ISystem
+    {
+        private readonly UnsubscribeHandler _unsubscribeHandler = new();
+
+        public bool Initialized { get; private set; }
+
+        void ISystem.Initialize()
+        {
+            if (Initialized)
+                return;
+
+            this.InjectDependency(this);
+            Initialize();
+            Initialized = true;
+        }
+
+        void ISystem.Cleanup()
+        {
+            Cleanup();
+            Initialized = false;
+        }
+
+        void ISystem.TrackUnsubscriber(IUnsubscriber unsubscriber)
+        {
+            _unsubscribeHandler.AddUnsubscriber(unsubscriber);
+        }
+
+        protected abstract void Initialize();
+        protected virtual void Cleanup() { }
+    }
+}
