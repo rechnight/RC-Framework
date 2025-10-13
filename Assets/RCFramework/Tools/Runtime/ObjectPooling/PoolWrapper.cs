b72bc8e40c7bf932d5bdeef357933f48444a5335
rechnight <57619271+rechnight@users.noreply.github.com>
2025-10-13 13:53:18 -0300

    Add files via upload

---
+++ Assets/RCFramework/Tools/Runtime/ObjectPooling/PoolWrapper.cs
@@ -0,0 +1,52 @@
+// --------------- Copyright (C) RC --------------- //
+// STRONG is what happens when you run out of weak! //
+
+using System;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace RCFramework.Tools
+{
+    public class PoolWrapper<T> : IPoolWrapper<T> where T : class, IPoolable<T>
+    {
+        private readonly IObjectPool<T> _objectPool;
+
+        public PoolWrapper(GameObject prefab)
+        {
+            _objectPool = new ObjectPool<T>(
+                createFunc: () => GameObject.Instantiate(prefab).GetComponent<T>()
+            );
+        }
+
+        public PoolWrapper(Func<T> onCreate, Action<T> onGet = null, Action<T> onRelease = null,
+            Action<T> onDestroy = null, bool collectionCheck = true, int defaultCapacity = 10, int maxSize = 100)
+        {
+            _objectPool = new ObjectPool<T>(
+                createFunc: () => onCreate(),
+                actionOnGet: obj => onGet?.Invoke(obj),
+                actionOnRelease: obj => onRelease?.Invoke(obj),
+                actionOnDestroy: obj => onDestroy?.Invoke(obj),
+                collectionCheck: collectionCheck,
+                defaultCapacity: defaultCapacity,
+                maxSize: maxSize
+            );
+        }
+
+        public T Get()
+        {
+            var obj = _objectPool.Get();
+            obj.Initialize(Release);
+            return obj;
+        }
+
+        public void Release(T obj)
+        {
+            _objectPool.Release(obj);
+        }
+
+        public void Clear()
+        {
+            _objectPool.Clear();
+        }
+    }
+}
