03ae0c75b8adfa6c94f9ca54a0ddb806a217df15
rechnight <57619271+rechnight@users.noreply.github.com>
2025-10-14 17:59:37 -0300

    Create ISaveable.cs

---
+++ Assets/RCFramework/Tools/Runtime/PersistentData/ISaveable.cs
@@ -0,0 +1,10 @@
+// --------------- Copyright (C) RC --------------- //
+// STRONG is what happens when you run out of weak! //
+
+namespace RCFramework.Tools
+{
+    public interface ISaveable 
+    {
+
+    }
+}
