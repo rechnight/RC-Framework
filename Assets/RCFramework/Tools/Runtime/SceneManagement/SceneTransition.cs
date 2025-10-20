7de89105f7ae44a8767df8b45d1293abcceef3c9
rechnight <57619271+rechnight@users.noreply.github.com>
2025-10-20 16:03:30 -0300

    Rename TransitionBuilder to SceneTransition

--- Assets/RCFramework/Tools/Runtime/SceneManagement/SceneTransition.cs
+++ Assets/RCFramework/Tools/Runtime/SceneManagement/SceneTransition.cs
@@ -5,43 +5,53 @@ using System.Collections.Generic;
 
 namespace RCFramework.Tools
 {
-    public class TransitionBuilder
+    public class SceneTransition
     {
         private readonly SceneLoader _sceneLoader;
 
         public Dictionary<SceneSlot, SceneKey> ScenesToLoad { get; } = new();
         public List<SceneSlot> ScenesToUnload { get; } = new();
-        public SceneKey ActiveSceneName { get; private set; }
+        public SceneKey ActiveSceneKey { get; private set; }
         public bool UnloadUnusedAssets { get; private set; } = false;
         public bool FadeOverlay { get; private set; } = false;
 
-        public TransitionBuilder(SceneLoader sceneLoader) => _sceneLoader = sceneLoader;
+        public SceneTransition(SceneLoader sceneLoader) => _sceneLoader = sceneLoader;
 
         public void Execute() => _sceneLoader.Execute(this);
 
-        public TransitionBuilder Load(SceneSlot slot, SceneKey sceneName, bool setActive = false)
+        public SceneTransition Load(SceneSlot slot, SceneKey sceneKey, bool setActive = false)
         {
-            ScenesToLoad[slot] = sceneName;
+            ScenesToLoad[slot] = sceneKey;
             if (setActive)
             {
-                ActiveSceneName = sceneName;
+                ActiveSceneKey = sceneKey;
             }
             return this;
         }
 
-        public TransitionBuilder Unload(SceneSlot slot)
+        public SceneTransition Load(ISceneData sceneData, bool setActive = false)
+        {
+            return Load(sceneData.SceneSlot, sceneData.SceneKey, setActive);
+        }
+
+        public SceneTransition Load(SceneSlot slot, string sceneName, bool setActive = false)
+        {
+            return Load(slot, (SceneKey)sceneName, setActive);
+        }
+
+        public SceneTransition Unload(SceneSlot slot)
         {
             ScenesToUnload.Add(slot);
             return this;
         }
 
-        public TransitionBuilder WithOverlay(bool fade)
+        public SceneTransition WithOverlay(bool fade)
         {
             FadeOverlay = fade;
             return this;
         }
 
-        public TransitionBuilder WithUnloadUnusedAssets(bool unload)
+        public SceneTransition WithUnloadUnusedAssets(bool unload)
         {
             UnloadUnusedAssets = unload;
             return this;
